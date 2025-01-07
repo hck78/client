8bd32736011ddcb3781083168f01591a856c17b0
Ivan <ijtj@Ivans-MacBook-Air.local>
2025-01-07 13:46:02 +0700

    feat: Nambahin user online di App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [userOnline, setUserOnline] = useState([])
 
   function handleIncrement() {
     setCount(c => c + 1)
   }
+
+  function fetchUserOnline() {
+
+  }
+
+  useEffect(() => {
+    fetchUserOnline()
+  }, [])
 
   return (
     <>
@@ -27,6 +36,11 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      {
+        userOnline((user) => {
+          return <li>{user.name}</li>
+        })
+      }
     <h1>{count}</h1>
     <button onClick={handleIncrement}>Increment</button>
     </>
